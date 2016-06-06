091f767929a1003b4a7bd86d82b892cf8d3780e8
rahulrainahome <rahulrainahome@live.com>
2016-06-06 22:20:57 +0530

    cloud web service url domain updated.

--- app/src/main/java/com/pro/ownerapp/util/Constant.java
+++ app/src/main/java/com/pro/ownerapp/util/Constant.java
@@ -1,8 +1,6 @@
 package com.pro.ownerapp.util;
 
-/**
- * @author Swapnil
- */
+
 public class Constant {
 
     public static final String DEFAULT_CHARSET = "UTF-8";
@@ -10,5 +8,5 @@ public class Constant {
     public static final String IS_REGISTERED = "IS_REGISTERED";
     public static final String REGISTERED_PASSWORD = "REGISTERED_PASSWORD";
 
-    public static String SEND_GCM_WEB_URL = "http://10.0.0.9:8080/ProMobileWebService/sendGCM/push";
+    public static String SEND_GCM_WEB_URL = "http://rahul.mulkindia.in/ProMobileWebService/sendGCM/push";
 }
