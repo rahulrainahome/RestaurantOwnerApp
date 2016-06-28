b30a28b6c6b7de919a5644e8cbcb4bbeec350dd4
rahulrainahome <rahulrainahome@live.com>
2016-06-28 20:43:46 +0530

    changes... Local IP

--- app/src/main/java/com/pro/ownerapp/util/Constant.java
+++ app/src/main/java/com/pro/ownerapp/util/Constant.java
@@ -8,5 +8,7 @@ public class Constant {
     public static final String IS_REGISTERED = "IS_REGISTERED";
     public static final String REGISTERED_PASSWORD = "REGISTERED_PASSWORD";
 
-    public static String SEND_GCM_WEB_URL = "http://rahul.mulkindia.in/ProMobileWebService/sendGCM/push";
+    public static String SEND_GCM_WEB_URL = "http://192.168.1.2:8080/ProMobileWebService/sendGCM/push";
+
+    //public static String SEND_GCM_WEB_URL = "http://rahul.mulkindia.in/ProMobileWebService/sendGCM/push";
 }
